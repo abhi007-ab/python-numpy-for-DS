e2a8eb7df476b85324287ca7f87d34453f338bd9
abhi007-ab <228505534+abhi007-ab@users.noreply.github.com>
2025-10-10 14:14:36 +0000

    arithmetic

--- Introduction To Numpy/Arithmetic/main.py
+++ Introduction To Numpy/Arithmetic/main.py
@@ -21,3 +21,29 @@ print(np.round(array))  # [1. 2. 4.]
 print(np.floor(array))  # [1. 2. 3.]
 print(np.ceil(array))   # [2. 3. 4.]
 print(np.pi)            # 3.141592653589793
+
+
+
+# Element-wise arithmetic
+
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+
+print(array1 + array2)    # [5 7 9]
+print(array1 - array2)    # [-3 -3 -3]
+print(array1 * array2)    # [ 4 10 18]
+print(array1 / array2)    # [0.25 0.4  0.5 ]
+print(array1 ** array2)   # [  1  32 729]
+
+
+
+# Comparison operators
+
+scores = np.array([91, 55, 100, 73, 82, 64])
+
+print(scores == 100)   # [False False  True False False False]
+print(scores >= 60)    # [ True False  True  True  True  True]
+print(scores < 60)     # [False  True False False False False]
+
+scores[scores < 60] = 0
+print(scores)    # [ 91   0 100  73  82  64]
