5bd224c15717c9771362258f496aed356c12929a
abhi007-ab <228505534+abhi007-ab@users.noreply.github.com>
2025-10-17 14:21:52 +0000

    v,m,r

---
+++ Introduction To Numpy/Vector,Matrix and Tensor/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# Vectors
+
+vector = np.array([1, 2, 3])
+print("Vectors: ", vector)
+
+
+# Matrix
+
+matrix = np.array([[1, 2, 3],
+                   [4, 5, 6]])
+print("Matrix: ", matrix)
+
+
+
+# Tensor
+
+tensor = np.array([[[1,2], [3, 4]],
+                   [[5, 6], [7, 8]]])
